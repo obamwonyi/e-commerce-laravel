c193633d292397108cb7d6488ab24f12df8fa42c
obamwonyi <developer.oduwa@gmail.com>
2022-09-25 23:34:05 +0100

    added the landing page layout header and footer with responsive navigations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
         <title>@yield('title', 'Online Store')</title>
     </head>
     <body>
@@ -13,7 +14,10 @@
                 <a class="navbar-brand" href="#">Online Store</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggOnline Store
+Home
+About
+A Laravel Online Storeler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav ms-auto">
@@ -32,7 +36,21 @@
         <div class="container my-4">
         @yield('content')
         </div>
+
+        <!-- footer -->
+    <div class="copyright py-4 text-center text-white">
+        <div class="container">
+            <small>
+                Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+                href="https://twitter.com/danielgarax">
+                Daniel Correa
+                </a> - <b>Paola Vallejo</b>
+            </small>
+        </div>
+    </div>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
         </script>
+        
     </body>
 </html>
