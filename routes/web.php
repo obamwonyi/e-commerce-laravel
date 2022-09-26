e1440656d27e1b9faa6b5a355e59600ab1e7c5c3
obamwonyi <developer.oduwa@gmail.com>
2022-09-26 11:01:27 +0100

    added dummy ui for the home and about page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//one way of defining a route in laravel 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    $viewData = [];
+    $viewData['title'] = "Home Page - Online Store";
+    return view('home.index')->with("viewData",$viewData);
 });
+
+//another way of defining a route in laravel . 
+//'App\Http\Controllers\HomeController@about 
+//the route Controller above means 
+//the HomeController.php will be executed  calling the 
+//about() method in the class. 
+Route::get('/about','App\Http\Controllers\HomeController@about')->name('home.about');
+//where first parameter is the url to look out for 
+//second parameter is the controller 
+//this will return a static instance that can extend the named route 
+// with the name() method 
